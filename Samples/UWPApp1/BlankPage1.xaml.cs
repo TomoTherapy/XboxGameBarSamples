b0bb649223ab2fc7a8f60b7b2ca3513412813449
홍준기 <wnsrlghd@gmail.com>
2022-10-04 18:00:46 +0900

    UWP created

---
+++ Samples/UWPApp1/BlankPage1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        private bool flag = false;
+        private XboxGameBarWidget widget;
+
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            widget = e.Parameter as XboxGameBarWidget;
+
+            widget.SettingsClicked += Widget_SettingsClicked;
+            //base.OnNavigatedTo(e);
+        }
+
+        private async void Widget_SettingsClicked(XboxGameBarWidget sender, object args)
+        {
+            await sender.ActivateSettingsAsync();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+                TextBox1.Text = "Jesus Fuck";
+            else
+                TextBox1.Text = "Allah Fuck";
+
+            flag = !flag;
+        }
+    }
+}
